39e288a9833e0c7d8c1dcd94b4fbd786f137f985
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-04 16:45:07 +0800

    Install Dependencies & Basic Server Setup

---
+++ server.js
@@ -0,0 +1,10 @@
+//bring what we need first
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello, World!'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
