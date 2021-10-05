896334392f604c70a2e9a82b33df42433204e802
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-05 13:50:59 +0800

    Connecting to MongoDB with mongoose

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+//para maging accessible yung mga data dito kahit sa ibang file
+module.exports = {
+    mongoURI: 'mongodb+srv://celine:celine123@devcord.r0f9c.mongodb.net/DevCord?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 //bring what we need first
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//DB Config
+const db = require('./config/keys').mongoURI
+
+//Connect to Mongodb
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error(err))
 
 app.get('/', (req, res) => res.send('Hello, World!'));
 
