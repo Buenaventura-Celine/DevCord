1fd58da85275b6bcb4c864f4ebab7fffc7670729
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-08 08:55:38 +0800

    User model created

---
+++ models/User.js
@@ -0,0 +1,29 @@
+//convention for models, singular and starts with capital
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+//create Schema
+const UserSchema = new Schema({
+    name: {
+        type: 'String',
+        required: true
+    },
+    email: {
+        type: 'String',
+        required: true
+    },
+    password: {
+        type: 'String',
+        required: true
+    },
+    avatar:{
+        type: 'String',
+        required: true
+    },
+    date: {
+        type: 'Date',
+        default: Date.now()
+    }
+})
+
+module.exports = User = mongoose.model('users', UserSchema)
