749a2c2b0279e51ee0b079fc9e22de97306511be
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-11 10:43:33 +0800

    If the email already exist

--- routes/api/users.js
+++ routes/api/users.js
@@ -16,7 +16,12 @@ router.get('/test', (req, res) => res.json({
 //@desc   Register user
 //@acces  Public
 router.post('/register', (req, res) => {
-    User.findOne({email: req.body.email})
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(user){
+                return res.status(400).json({ email: 'Email already exist' })
+            }
+        })
 })
 
 module.exports = router;
