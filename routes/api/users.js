572713cabdc76c11db15388e1b5a10390183caa7
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-11 17:54:46 +0800

    Hashing password

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
 
 //Load user model 
 const User = require('../../models/User')
@@ -33,6 +34,16 @@ router.post('/register', (req, res) => {
                     email : req.body.email,
                     avatar, 
                     password: req.body.password
+                })
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err))
+                    })
                 })
             }
         })
