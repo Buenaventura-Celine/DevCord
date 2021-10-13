2feb173361753329fbc2091fca6d1b4a7937bdf2
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-13 20:30:23 +0800

    Import key from config

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const keys = require('../../config/keys')
 
 //Load user model 
 const User = require('../../models/User')
