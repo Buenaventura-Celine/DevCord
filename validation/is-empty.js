3f9fb2349334ed9ee25e5f76a628c6ced9ab4d8c
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-15 21:09:36 +0800

    Created a file to check if the input is empty

---
+++ validation/is-empty.js
@@ -0,0 +1,7 @@
+const isEmpty = value =>
+    value === undefined || 
+    value === null
+    (typeof value === 'object' && Object.keys(value).length === 0) || //check to see if the object is not empty
+    (typeof value === 'string' && value.trim().length === 0);
+
+module.exports = isEmpty;
