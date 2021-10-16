15b0685231afcaa0e31e9e3a3afed441d528b135
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-16 09:03:39 +0800

    Validation for login is implemented in the route

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,6 +12,7 @@ const User = require('../../models/User')
 
 //Load input validation
 const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
 
 //@route  GET api/user/test
 //@desc   Tests user route
@@ -66,6 +67,13 @@ router.post('/register', (req, res) => {
 //@desc   Login user/Returning JWT Token
 //@acces  Public
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body)
+
+    //Check validation
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -74,7 +82,8 @@ router.post('/login', (req, res) => {
         .then(user => {
             //Check for user 
             if(!user){
-                return res.status(404).json({email: 'User not found'})
+                errors.email = 'User not found'
+                return res.status(404).json(errors)
             }
 
             //Check password
@@ -100,7 +109,8 @@ router.post('/login', (req, res) => {
                                 })
                             })
                     }else{
-                        return res.status(400).json({ password: 'Incorrect Password' })
+                        errors.password = 'Incorrect password'
+                        return res.status(400).json(errors)
                     }
                 })
         })
