9673b9c91c87fce6b2b64129a5e2e7f17c73ce3e
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-16 09:05:11 +0800

    Fixed some bugs

--- validation/login.js
+++ validation/login.js
@@ -10,12 +10,12 @@ module.exports = function validateLoginInput(data){
 
     
     //add another layer for validation
-    if(Validator.isEmpty(data.email)){
-        errors.email = 'Email field is required'
-    }
-
     if(!Validator.isEmail(data.email)){
         errors.email = 'Email is invalid'
+    }
+
+    if(Validator.isEmpty(data.email)){
+        errors.email = 'Email field is required'
     }
 
     if(Validator.isEmpty(data.password)){
