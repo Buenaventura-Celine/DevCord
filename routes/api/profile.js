fab25adf236185c4512ecbd99bf3a267ca469755
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-18 12:10:32 +0800

    Add current user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -17,6 +17,20 @@ router.get('/test', (req, res) => res.json({
     message: 'Profile Route work'
 }))
 
-
+//@route  GET api/profile/
+//@desc   Get current user profile
+//@acces  Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const errors = {}
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if(!profile){
+                errors.noprofile = 'There is no profile for this user'
+                return res.status(404).json(errors)
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err))
+})
 
 module.exports = router;
