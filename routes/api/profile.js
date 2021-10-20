999a04aeb66dce194880d8cf34abd2b70590d65f
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-20 09:10:05 +0800

    Create profile - check if handle exist

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -74,6 +74,14 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
                 )
             }else{
                 //Create
+                //Check if handle exists
+                Profile.findOne({ handle: profileFields.handle })
+                    .then(profile => {
+                        if(profile){
+                            errors.handle = 'That handle already exists'
+                            res.status(400).json(errors)
+                        }
+                    })
             }
         })
 })
