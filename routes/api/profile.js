ed761b45a820e3634342dcb9805b51d8c0b4a25d
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-20 08:48:32 +0800

    Declaring profile fields

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -33,4 +33,12 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
         .catch(err  => res.status(404).json(err))
 })
 
+//@route  POST api/profile/
+//@desc   Create user profile
+//@acces  Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    //Get fields 
+    const profileFields = {};
+})
+
 module.exports = router;
