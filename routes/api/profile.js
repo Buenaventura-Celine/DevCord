d81a68474d99fc703b05761e96c917d400075c00
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-21 15:00:54 +0800

    Check validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -40,6 +40,11 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
     const { errors, isValid } = validateProfileInput(req.body)
 
+    //Check Validation
+    if(!isValid){
+        //Return any errors with 400 status
+        return res.status(400).json(errors)
+    }
 
     //Get fields 
     const profileFields = {};
