407b11c0284739b99ef90a480cdea7ac61731eff
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-21 13:48:09 +0800

    Initializing validator for profile

---
+++ validation/profile.js
@@ -0,0 +1,29 @@
+const Validator = require('validator')
+const isEmpty = require('./is-empty')
+
+module.exports = function validateProfileInput(data){
+    let errors = {}
+
+    //another layer to check is the data is not empty
+    data.email = !isEmpty(data.email) ? data.email : '';
+    data.password = !isEmpty(data.password) ? data.password : '';
+
+    
+    //add another layer for validation
+    if(!Validator.isEmail(data.email)){
+        errors.email = 'Email is invalid'
+    }
+
+    if(Validator.isEmpty(data.email)){
+        errors.email = 'Email field is required'
+    }
+
+    if(Validator.isEmpty(data.password)){
+        errors.password = 'Password field is required'
+    }
+
+    return {
+        errors: errors,
+        isValid: isEmpty(errors),
+    }
+}
