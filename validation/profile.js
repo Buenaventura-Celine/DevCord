ad0d691b696069b65671303f40033d4cb34d37c1
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-21 14:21:21 +0800

    Completed profile validator

--- validation/profile.js
+++ validation/profile.js
@@ -4,22 +4,60 @@ const isEmpty = require('./is-empty')
 module.exports = function validateProfileInput(data){
     let errors = {}
 
-    //another layer to check is the data is not empty
-    data.email = !isEmpty(data.email) ? data.email : '';
-    data.password = !isEmpty(data.password) ? data.password : '';
+    data.handle = !isEmpty(data.handle) ? data.handle : '';
+    data.status = !isEmpty(data.status) ? data.status : '';
+    data.skills = !isEmpty(data.skills) ? data.skills : '';
 
-    
-    //add another layer for validation
-    if(!Validator.isEmail(data.email)){
-        errors.email = 'Email is invalid'
+    if(!Validator.isLength(data.handle, { min: 2, mas: 40 })){
+        errors.handle = 'Handle needs to between 2 and 4 characters';
     }
 
-    if(Validator.isEmpty(data.email)){
-        errors.email = 'Email field is required'
+    if(Validator.isEmpty(data.handle)){
+        errors.handle = 'Profile handle is required'
     }
 
-    if(Validator.isEmpty(data.password)){
-        errors.password = 'Password field is required'
+    if(Validator.isEmpty(data.status)){
+        error.status = 'Status field is required'
+    }
+
+    if(Validator.isEmpty(data.skills)){
+        error.skills = 'Skills field is required'
+    }
+
+    if(!isEmpty(data.website)){
+        if(!Validator.isURL(data.website)){
+            errors.website = 'Not a valid URL'
+        }
+    }
+
+    if(!isEmpty(data.youtube)){
+        if(!Validator.isURL(data.youtube)){
+            errors.youtube = 'Not a valid URL'
+        }
+    }
+
+    if(!isEmpty(data.twitter)){
+        if(!Validator.isURL(data.twitter)){
+            errors.twitter = 'Not a valid URL'
+        }
+    }
+
+    if(!isEmpty(data.facebook)){
+        if(!Validator.isURL(data.facebook)){
+            errors.facebook = 'Not a valid URL'
+        }
+    }
+
+    if(!isEmpty(data.linkedin)){
+        if(!Validator.isURL(data.linkedin)){
+            errors.linkedin = 'Not a valid URL'
+        }
+    }
+
+    if(!isEmpty(data.instagram)){
+        if(!Validator.isURL(data.instagram)){
+            errors.instagram = 'Not a valid URL'
+        }
     }
 
     return {
