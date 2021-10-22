b35626702011a92e9041c8cf29ff50995e4acb14
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-22 16:32:15 +0800

    Next step after the validation for profile existence

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,13 +39,17 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 //@desc   Get profile by handle
 //@acces  Public
 router.get('/handle/:handle', (req, res) => {
+    const errors = {};
     Profile.findOne({ handle: req.params.handle })
         .populate('user', ['name', 'avatar'])
         .then(profile => {
             if(!profile){
                 errors.noprofile = 'There is no profile for this user'
+                res.status(404).json(errors)
             }
+            res.json(profile)
         })
+        .catch(err => res.status(404).json(err))
 })
 
 //@route  POST api/profile/
