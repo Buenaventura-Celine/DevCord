c041a04b13d8980959f03e0d65fa368ecad6445e
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-22 16:43:25 +0800

    Get profile by user id route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -52,6 +52,23 @@ router.get('/handle/:handle', (req, res) => {
         .catch(err => res.status(404).json(err))
 })
 
+//@route  GET api/profile/user/:user_id 
+//@desc   Get profile by user id
+//@acces  Public
+router.get('/user/:user_id', (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.params.user_id })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if(!profile){
+                errors.noprofile = 'There is no profile for this user'
+                res.status(404).json(errors)
+            }
+            res.json(profile)
+        })
+        .catch(err => res.status(404).json(err))
+})
+
 //@route  POST api/profile/
 //@desc   Create or Edit user profile
 //@acces  Private
