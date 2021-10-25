26b6ecf651632099279a8e0515eedb1874049ee5
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-25 11:34:49 +0800

    Removing a user functionality completed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -275,7 +275,11 @@ router.delete('/education/:edu_id', passport.authenticate("jwt", { session: fals
 //@desc   Delete user and profile
 //@acces  Private
 router.delete('/', passport.authenticate("jwt", { session: false }), (req, res) => {
-
+    Profile.findOneAndRemove({ user: req.user.id })
+        .then(() => {
+            User.findOneAndRemove({ _id: req.user.id })
+                .then(() => res.json({ success: true }))
+        })
 }
 );
 
