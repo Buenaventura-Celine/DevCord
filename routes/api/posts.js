58767dd58197d7f02a02fbfef4e94ceb7aa583d3
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-10-28 08:16:57 +0800

    Bringing in the necessary modules in posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,6 +2,8 @@
 //just going to deal with authentication, username, email, and password.
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose') //because where dealing with database
+const passport = require('passport') //for our protected route
 
 //@route  GET api/posts/test
 //@desc   Tests post route
