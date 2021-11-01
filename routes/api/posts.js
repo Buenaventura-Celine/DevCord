cc913b638951b8fae4e6ba75fd4942ff42c6f95a
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-11-01 09:52:37 +0800

    Delete comment route completed

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -157,9 +157,19 @@ router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session
     Post.findById(req.params.id)
         .then(post => {
             //Check if the comment exists 
-            if (post.comments.filter(comment => comment._id.toString() === req.params.comment_id.length === 0)) {
-
+            if (post.comments.filter(comment => comment._id.toString() == req.params.comment_id).length == 0) {
+                return res.status(404).json({ commentnotexists: 'Comment does not exist' })
             }
+
+            //Get remove index
+            const removeIndex = post.comments
+                .map(item => item._id.toString())
+                .indexOf(req.params.comment_id)
+
+            //Splice comment out of arrays
+            post.comments.splice(removeIndex, 1)
+
+            post.save().then(post => res.json(post))
         })
         .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
 })
