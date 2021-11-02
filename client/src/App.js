aa6cdbc20d4f285b5826174e8d36bb8c43103082
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-11-02 09:18:21 +0800

    Navbar created

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
 
@@ -9,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Landing />
         <Footer />
       </div >
