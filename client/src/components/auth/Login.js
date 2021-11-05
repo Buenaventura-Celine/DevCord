d57432b9e01d098990993acedd9906cde6d74853
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-11-05 15:58:03 +0800

    Login Layout

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -3,8 +3,35 @@ import React, { Component } from 'react'
 class Login extends Component {
     render() {
         return (
-            <div>
-                <h1>Login</h1>
+            <div className="login">
+                <section className="container">
+                    <div className="alert alert-danger">
+                        Invalid credentials
+                    </div>
+                    <h1 className="large text-primary">Sign In</h1>
+                    <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+                    <form className="form" action="dashboard.html">
+                        <div className="form-group">
+                            <input
+                                type="email"
+                                placeholder="Email Address"
+                                name="email"
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                            />
+                        </div>
+                        <input type="submit" className="btn btn-primary" value="Login" />
+                    </form>
+                    <p className="my-1">
+                        Don't have an account? <a href="register.html">Sign Up</a>
+                    </p>
+                </section>
             </div>
         )
     }
