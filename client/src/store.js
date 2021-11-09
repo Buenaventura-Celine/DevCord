546ae54d773a55aa5c3e834d82e80408461198b3
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-11-09 09:47:50 +0800

    Initial State

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,9 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers' //mababasa na agad yung inddex.js
 
+const initialState = {}
+
 const middleware = [thunk]
-const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
+const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
 
 export default store;
