b2607644c59a862bea8d9cc76df27986719c82bd
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2021-11-09 09:47:06 +0800

    Root reducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import rootReducer from './reducers' //mababasa na agad yung inddex.js
 
 const middleware = [thunk]
-const store = createStore(() => [], {}, applyMiddleware(...middleware));
+const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
 
 export default store;
